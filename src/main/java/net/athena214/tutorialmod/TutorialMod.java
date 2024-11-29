81d98727c61f4fad61c4c82abca7a50942550998
athena214 <weatherkid10@gmail.com>
2024-11-28 18:50:20 -0600

    added some things

--- src/main/java/net/athena214/tutorialmod/TutorialMod.java
+++ src/main/java/net/athena214/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.athena214.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.athena214.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,6 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
